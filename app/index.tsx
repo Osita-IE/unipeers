0c4f9d8b2177b29bc6f800d0e54ee2ab21e72acc
Osita-IE <ezeositaisrael@gmail.com>
2025-08-04 10:09:12 -0700

    first page edit

--- app/index.tsx
+++ app/index.tsx
@@ -1,35 +1,60 @@
-import { introStyles } from "@/styles/intro.styles";
-import { ImageBackground, Text, TouchableOpacity, View } from "react-native";
+import { aboutStyles } from "@/styles/about.styles";
+import { Text, View } from "react-native";
 
-export default function index() {
+export default function About () {
   return (
-    <View style={introStyles.cover}>
-      <ImageBackground
-      style={introStyles.bg}
-        source={require("../assets/images/intro_bg.jpg")}
-      >
-        <View style={introStyles.layer}>
+    <View style={aboutStyles.wrapper}>
+      {/* header content */}
+      <View style={aboutStyles.header}>
+        <Text style={aboutStyles.title}>unipeers</Text>
+      </View>
 
-          <View>
-            <Text style={introStyles.title}>uniPeers</Text>
-          </View>
-
-          <View style={introStyles.body}>
-            <Text style={introStyles.bodyText}>The school app for socialising. Connect
-              with friends, create your own event, 
-              and find other amazing events from students.
-            </Text>
-          </View>
-
-          <View>
-            <TouchableOpacity style={introStyles.btn}>
-                <Text style={introStyles.btnText}>Get 
-                Started</Text>
-            </TouchableOpacity>
-          </View>
-
+      {/* middle content */}
+      <View style={aboutStyles.body}>
+        {/* first sub-group */}
+        <View style={aboutStyles.aboutBlock}>
+          <Text style={aboutStyles.aboutText}>Unipeers is a social media app made for tertiary
+             institution students to connect with friends and classmates.</Text>
+          <Text style={aboutStyles.aboutText}>On Unipeers you will find tons of events in your socials.
+            You can as well create your own events</Text>
+          <Text style={aboutStyles.aboutText}>It goes further! You can find events in other schools right from your own campus</Text>
         </View>
-      </ImageBackground>
+
+        {/* second sub-group */}
+        <View style={aboutStyles.featuresBlock}>
+            <View style={aboutStyles.featureBlock}>
+              <Text style={aboutStyles.featureTitle}>Events</Text>
+              <Text style={aboutStyles.featureText}>Create your own events</Text>
+            </View>
+            <View style={aboutStyles.featureBlock}>
+              <Text style={aboutStyles.featureTitle}>Post</Text>
+              <Text style={aboutStyles.featureText}>Interract with posts</Text>
+            </View>
+        </View>
+
+        {/* third sub-group */}
+        <View>
+            <View>
+              <Text>1m+ users</Text>
+            </View>
+            <View>
+              <Text>10m+ events</Text>
+            </View>
+            <View>
+              <Text>100m+ posts</Text>
+            </View>
+        </View>
+      </View>
+
+      {/* bottom content */}
+      <View>
+        <View>
+          <Text>Privacy Policy</Text>
+          <Text>Terms of Use</Text>
+          <Text>Contact Us</Text>
+          <Text>{new Date().getFullYear()} All Rights Reserved. Unipeers</Text>
+        </View>
+      </View>
     </View>
   )
 }
---
+++ components/home.tsx
@@ -0,0 +1,35 @@
+import { introStyles } from "@/styles/intro.styles";
+import { ImageBackground, Text, TouchableOpacity, View } from "react-native";
+
+export default function index() {
+  return (
+    <View style={introStyles.cover}>
+      <ImageBackground
+      style={introStyles.bg}
+        source={require("../assets/images/intro_bg.jpg")}
+      >
+        <View style={introStyles.layer}>
+
+          <View>
+            <Text style={introStyles.title}>uniPeers</Text>
+          </View>
+
+          <View style={introStyles.body}>
+            <Text style={introStyles.bodyText}>The school app for socialising. Connect
+              with friends, create your own event, 
+              and find other amazing events from students.
+            </Text>
+          </View>
+
+          <View>
+            <TouchableOpacity style={introStyles.btn}>
+                <Text style={introStyles.btnText}>Get 
+                Started</Text>
+            </TouchableOpacity>
+          </View>
+
+        </View>
+      </ImageBackground>
+    </View>
+  )
+}
