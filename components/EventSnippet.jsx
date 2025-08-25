216b2910267204ab835a4d10a985effedd1ae44d
Osita-IE <ezeositaisrael@gmail.com>
2025-08-25 18:11:22 +0100

    ll

--- components/EventSnippet.jsx
+++ components/EventSnippet.jsx
@@ -1,18 +1,61 @@
-import MaterialIcons from '@expo/vector-icons/MaterialIcons';
-import { Text, View } from "react-native";
-import { themeColors } from "../utils/theme.utils";
+import { events } from "@/assets/local-data/events";
+import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
+import { Link } from "expo-router";
+import { Dimensions, FlatList, Image, Text, View } from "react-native";
 
-export function EventSnippet ({ mainTitle,subTitle,iconName }) {
-    return (
-        <View className="w-full flex flex-row gap-x-8">
-            <MaterialIcons 
-            name={iconName} 
-            size={36}
-            style={{color: themeColors.darkGreen}} />
-            <View>
-                <Text style={{color: themeColors.darkGreen,fontWeight: "bold"}}>{mainTitle}</Text>
-                <Text>{subTitle}</Text>
+// calculate screen width
+const screenWidth = Dimensions.get("window").width;
+
+
+export default function Index () {
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView style={{
+        flex: 1
+      }}>
+        <FlatList
+        data={events}
+        ItemSeparatorComponent={Seperator}
+        renderItem={({item}) => {
+          return (
+            <View className="relative">
+              <Image
+              source={{uri: item.bannerUrl}}
+              alt="event photo"
+              style={{width:screenWidth,height:400,resizeMode:"cover"}}/>
+
+              {/* this view is set to absolute on event component */}
+              <View className=" absolute top-3 p-3 bg-teal-800 rounded-md">
+                {item.free === true 
+                ? 
+                <Text className="text-white font-bold">Free</Text>
+                :
+                <Text className="text-white font-bold">₦{item.fee}</Text>
+                }
+              </View>
+
+              <View className="px-3">
+                <View className="p-3 rounded-b-md border border-teal-600 border-t-0">
+                  <Text className="text-xs font-bold">{item.title}</Text>
+                  <View className="flex flex-row justify-between">
+                    <View className="flex flex-row items-center gap-x-2">
+                       <FontAwesome5 
+                       name="user-circle" 
+                       size={24} 
+                       color="black" />
+                       <Text>{item.createdBy}</Text>
+                    </View>
+                    <Link href= {`/event-details/${item.id}`} className="p-3 font-bold text-white bg-teal-800 rounded-md">
+                      <Text>Event details</Text>
+                    </Link>
+                  </View>
+                </View>
+              </View>
             </View>
-        </View>
-    )
+          )
+        }}
+        keyExtractor={(item) => item.id}/>
+      </SafeAreaView>
+    </SafeAreaProvider>
+  )
 }
---
+++ styled/custom-styled-components.js
@@ -0,0 +1,44 @@
+import styled from "styled-components/native";
+
+const P = styled.Text`
+    color: #000
+`;
+
+const Span = styled.Text`
+    color: #000
+`;
+
+const H1 = styled.Text`
+    color: #000
+`;
+
+const H2 = styled.Text`
+    color: #000
+`;
+
+const H3 = styled.Text`
+    color: #000
+`;
+
+const Main = styled.View`
+    padding: 0;
+    margin: 0;
+`;
+
+const Header = styled.View`
+    padding: 0;
+    margin: 0;
+`;
+
+const Division = styled.View`
+    padding: 0;
+    margin: 0;
+`;
+
+const Blockquote = styled.View`
+    padding: 0;
+    margin: 0;
+`;
+
+export { H1, H2, H3, Header, Main, Main, P, P, Span, Span };
+
